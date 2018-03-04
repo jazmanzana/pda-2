2dfece180a7f2eb032e3db5a82b3431295901483
jazmanzana <19Kiel12>
2018-03-04 19:55:21 -0300

    last changes

--- web_server.go
+++ web_server.go
@@ -6,11 +6,21 @@ import (
     "net/http"
 )
 
+var count int
+
 func main() {
+    http.HandleFunc("/count", counter)
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) { // por que pasa el puntero?
+    count++
     fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+    fmt.Fprint(w, "EEeeeEee")
+}
+
+func counter(w http.ResponseWriter, r *http.Request){
+    fmt.Fprint(w, "bUUUU")
+    fmt.Fprintf(w, "Count %d\n", count)
 }
