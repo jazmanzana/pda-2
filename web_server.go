c363b417ca3228a017448d324723daaa21a81567
jazmanzana <19Kiel12>
2018-03-06 00:06:39 -0300

    Chau canales, agrego valor de retorno para fetch.

--- web_server.go
+++ web_server.go
@@ -14,40 +14,31 @@ func main() {
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe("localhost:8000", nil))
 
-/*
-    ch := make(chan string)
-    for _, url := range os.Args[1:]{
-        go fetch(url, ch) // arranca la go routine -> fetch de las urls que mande por parametro
-    }
-    for range os.Args[1:]{
-        fmt.Println(<-ch) // recibe lo que venga de ch 
-        }*/
-
 }
 
-func fetch(url string, ch chan<- string){
+func fetch(url string) string {
     resp, err := http.Get(url)
     if err != nil {
-        ch <- fmt.Sprint(err) // por que usar sprint en vez de cualquiera de los otros prints?
-        return
+        fmt.Println("Errooooorrrrrr: ", err) // por que usar sprint en vez de cualquiera de los otros prints?
+        return "error"
     }
     fmt.Sprintf("This is resp.Body: %s", resp.Body)
-    nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+    nbytes, err := io.Copy(ioutil.Discard, resp.Body) // utilidad?
     resp.Body.Close() // "dont leak resources"
     if err != nil {
-        ch <- fmt.Sprintf("While reading %s: %v", url, err)
-        return
+        fmt.Println("While reading %s: %v", url, err)
+        return "error"
     }
-    ch <- fmt.Sprintf("Esto vendria a ser lo que recibimos: %d de la url %s", nbytes, url)
+    fmt.Println("Esto vendria a ser lo que recibimos: %d de la url %s", nbytes, url)
+    return "response"
 }
 
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    ch := make(chan string)
     dominio := "https://api.mercadolibre.com"
     my_url := strings.Join([]string{dominio, r.URL.Path}, "")
-    fmt.Println("Entraste a handler por la url %s", my_url)
-    fetch(my_url, ch)
+    fmt.Println("Entraste a handler por la url ", my_url) // consola
+    fmt.Fprintf(w, fetch(my_url))
 }
 
 
