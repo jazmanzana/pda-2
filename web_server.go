4f4083183fc76e449d9abebeb0df5673999723cf
jazmanzana <19Kiel12>
2018-03-08 20:52:40 -0300

    get request a API terminada

--- web_server.go
+++ web_server.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "io/ioutil"
-    //"os"
-    "io"
-    "strings"
+	"fmt"
+	"log"
+	"net/http"
+	//"io/ioutil"
+	//"os"
+	//"io"
+	"strings"
 )
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
 
 }
 
-func fetch(url string) string {
-    resp, err := http.Get(url)
-    if err != nil {
-        fmt.Println("Errooooorrrrrr: ", err) // por que usar sprint en vez de cualquiera de los otros prints?
-        return "error"
-    }
-    fmt.Sprintf("This is resp.Body: %s", resp.Body)
-    nbytes, err := io.Copy(ioutil.Discard, resp.Body) // utilidad?
-    resp.Body.Close() // "dont leak resources"
-    if err != nil {
-        fmt.Println("While reading %s: %v", url, err)
-        return "error"
-    }
-    fmt.Println("Esto vendria a ser lo que recibimos: %d de la url %s", nbytes, url)
-    return "response"
+// You need to specify what you will return after specifying the input parameters, this is not python.
+func fetch(url string, writer http.ResponseWriter) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Errooooorrrrrr: ", err) // por que usar sprint en vez de cualquiera de los otros prints?
+	}
+
+	body_bytes := make([]byte, resp.ContentLength)
+
+	_, err = resp.Body.Read(body_bytes) // _ ignora
+	if err != nil {
+		fmt.Println("While reading %s: %v", url, err)
+	}
+	//fmt.Println("Esto vendria a ser lo que recibimos: %d de la url %s", nbytes, url)
+	writer.Write(body_bytes)
+	resp.Body.Close() // "dont leak resources"
+
 }
 
+func handler(writer http.ResponseWriter, request *http.Request) {
+	dominio := "https://api.mercadolibre.com"
+	my_url := strings.Join([]string{dominio, request.URL.Path}, "")
+	fetch(my_url, writer)
+	//request.Write(writer)
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    dominio := "https://api.mercadolibre.com"
-    my_url := strings.Join([]string{dominio, r.URL.Path}, "")
-    fmt.Println("Entraste a handler por la url ", my_url) // consola
-    fmt.Fprintf(w, fetch(my_url))
 }
-
-
