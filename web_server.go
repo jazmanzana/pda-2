0dd5a55ee8fc39cebca10c09a23beb45ed16beb9
jazmanzana <19Kiel12>
2018-03-09 09:25:39 -0300

    guarda en el map las diferentes url_paths de clientes con la misma ip

--- web_server.go
+++ web_server.go
@@ -11,14 +11,19 @@ import (
     "errors"
 )
 
-    
-type client struct {
-    RemoteAddr string //ip del chabon
-    UrlPath string 
-    Counter int 
+/* Pongo client con mayusculas porque esta en el tutorial asi,
+lo que suecede es que puedo usar el struct Client desde otros modulos.
+Si estuviese en minuscula le cambia el scope a este lugar. */    
+type Client struct { 
+    Remote_Addr string //ip del chabon, no me sirve aca porque ya tengo la referencia en clients
+    URL_Path map[string]int // my key es el path, mi valor es el counter
 }
 
+
+var clients map[string]*Client
+
 func main() {
+    clients = make(map[string]*Client) //inicializo clients
 	http.HandleFunc("/", handler)
     http.HandleFunc("/statistics", get_statistics) //para las estadisticas luego
 	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
@@ -68,6 +73,26 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func request_restrictions(writer http.ResponseWriter, request *http.Request) error {
+    client_data, ok := clients[request.RemoteAddr]
+    if !ok { // si no lo encuentra, lo crea
+        clients[request.RemoteAddr] = &Client{
+            Remote_Addr: request.RemoteAddr, 
+            URL_Path: make(map[string]int)}
+        clients[request.RemoteAddr].URL_Path[request.URL.Path] = 1 //inicializa el path y pone el counter en 1
+        fmt.Println("Client no existia.")
+        return nil // si no existia no hay restricciones
+    }
+    
+    my_count, ok := clients[request.RemoteAddr].URL_Path[request.URL.Path]
+    if !ok { // si mi path no existe en mi client, lo crea
+        clients[request.RemoteAddr].URL_Path[request.URL.Path] = 1
+        fmt.Println("URL_Path no existia.")
+        return nil // no existia, no hay restricciones
+    }
+
+    fmt.Println(client_data)
+    fmt.Println(my_count)
+
     return nil
 }
 
